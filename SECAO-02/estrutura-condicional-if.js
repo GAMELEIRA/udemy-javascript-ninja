6e5cc4903df43361a2080683a0448d361c8045b4
Gabriel Gameleira dos Santos <gabriel.gsantos@totvs.com.br>
2021-01-06 15:20:09 -0300

    Atualizando a seção dois do curso sem subir o desafio

---
+++ SECAO-02/estrutura-condicional-if.js
@@ -0,0 +1,27 @@
+var x = 3;
+
+var y = 7;
+
+console.log(`y = ${y}`);
+
+console.log(`y = ${x}`);
+
+if (x === 3) {
+
+    y = 3;
+
+} else if (x === y && x === 3) {
+
+    x = 5;
+    y = 2;
+
+} else {
+
+    console.log('x não é igual a 3.');
+
+}
+
+
+console.log(`y = ${y}`);
+
+console.log(`y = ${x}`);
---
+++ SECAO-02/operadores-logicos.js
@@ -0,0 +1,9 @@
+var x = 3;
+
+var y = 7;
+
+console.log(x === 3 && 3 > 2);
+
+console.log(x === 3 || 3 < 2);
+
+console.log(!(x === 3));
